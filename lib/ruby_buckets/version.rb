43f1ec3d391f21f9be0ebad78aec4e35c7df74f1
Brian Rahming <admin@exusiasoftware.com>
2018-04-29 19:28:05 -0700

    new commit

---
+++ lib/ruby_buckets/version.rb
@@ -0,0 +1,3 @@
+module RubyBuckets
+  VERSION = "0.1.0"
+end
---
+++ spec/ruby_buckets_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe RubyBuckets do
+  it "has a version number" do
+    expect(RubyBuckets::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
